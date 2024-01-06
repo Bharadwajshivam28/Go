8d9e6f0dd9d8a277be571ecf933c1c2874266017
Shivam Kumar <shivambharwdajshivam28>
2024-01-06 16:24:37 +0530

    Created a Go code to uplaod files on Slack

---
+++ Slack_Bot_For_File_Upload/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5869019869751-6432568343108-EWER18aDaq6yXTpkfzhzDFov")
+	os.Setenv("CHANNEL_ID", "C05RWJMGD9B")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"test.yml", "test2.yml"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+}
