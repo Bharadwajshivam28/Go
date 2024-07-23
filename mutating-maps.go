e7d71438408d07ec9381b3443f4497381e14fbf1
Shivam <shivambharadwaj822@gmail.com>
2024-07-23 07:48:37 +0530

    Go

---
+++ mutating-maps.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("Value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("Valu:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("Value:", m["Answer"])
+ 
+	v, ok := m["Answer"]
+	fmt.Println("Value:", v, "Present?", ok)
+
+}
